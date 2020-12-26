57087ee233ee8f7bd055514de40be75e39873076
Julien <julien.rahier@gmail.com>
2020-12-26 02:27:34 +0100

    add build

--- src/components/challenge/index.js
+++ src/components/challenge/index.js
@@ -3,8 +3,3 @@ import React from 'react';
 export default function Challenge({children, ...restProps}) {
   return <Container {...restProps}>{children}</Container>;
 }
-
-Challenge.Title = 
-
-Challenge.Desc = 
-
